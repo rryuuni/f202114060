c6ce447aecaf2f3fe1dd56a506a8df1124ec4f2e
rryuuni <fkdbs1122@naver.com>
2024-10-16 09:21:00 +0900

    five

--- f202114060/World.java
+++ f202114060/World.java
@@ -3,6 +3,10 @@ public class World {
     public static void run() {
       System.out.println("202114060");
     }
+    public static void run2() {
+        System.out.println("hello world");
+      }
+    
   
   }
   
